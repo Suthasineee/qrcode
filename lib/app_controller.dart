f5713c6622e6b1f8daa5b333d7b088a144264d74
suthasineeSangchan <86451607+suthasineeSangchan21@users.noreply.github.com>
2025-09-17 10:53:29 +0700

    add

---
+++ lib/app_controller.dart
@@ -0,0 +1,8 @@
+const fontMitr = 'Mitr';
+
+const apiKey = 'AIzaSyDglx0hRy-8sEs-1acm3X_G7wSYG-1YFMU';
+const appId = '1:719714508765:web:5b21bb5a5111f290f12034';
+const messagingSenderId = '719714508765';
+const projectId = 'qrcode-1234';
+
+    
---
+++ lib/homepage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+class HomePage extends StatefulWidget {
+  HomePage();
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  setstate() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center());
+  }
+}
